1cbd49174eb1fa17e779aa69b4045cb3c2199fe2
kannanabhi <kannanabhistudies@gmail.com>
2022-02-09 22:50:22 +0530

    myalloc and myfree complete

--- custom_myalloc_myfree/driver.c
+++ custom_myalloc_myfree/driver.c
@@ -4,15 +4,19 @@
 
 long int total_heap_space=0;
 
-void *myalloc(int n) {
-    void *ptr=malloc(n);
+void *myalloc(size_t n) {
+    n++;
+    int *ptr=malloc(n);
+    ptr[0]=n;
     total_heap_space+=n;
     return ptr;
 }
 
-void myfree(int *p,int x) {
+void myfree(int *p) {
+    int size=p[0];
     free(p);
-    total_heap_space-=x;
+
+    total_heap_space-=size;
 }
 
 int main() {
@@ -21,7 +25,11 @@ int main() {
     for(int i=0;i<10;i++) {
         int M=(rand()%(25000-10000+1))+10000;
         int *A=myalloc(M*sizeof(int));
+        if(A==NULL) {
+            printf("\nproblem initialising pointer\n");
+            break;
+        }
         printf("%p %p \n",A,&A[M-1]);
-        myfree(A,M*sizeof(int));
+        myfree(A);
     }
 }
