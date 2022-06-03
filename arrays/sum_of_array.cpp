54cb81370e937950a5e452bfa0f254fcec90e288
kannan <kannanabhistudies@gmail.com>
2022-06-03 15:36:07 +0530

    sum of elements in array is complete

--- arrays/sum_of_array.cpp
+++ arrays/sum_of_array.cpp
@@ -2,8 +2,27 @@
 using namespace std;
 
 void array_init(int arr[],int size) {
-
+    for(int i=0;i<size;i++) {
+        cin>>arr[i];
+    }
 }
+int arraySum(int arr[],int size) {
+    int sum=0;
+    for(int i=0;i<size;i++) {
+        sum+=arr[i];
+    }
+    return sum;
+}
+
+void printArr(int arr[],int size) {
+    for(int i=0;i<size;i++) {
+        cout<<arr[i]<<" ";
+    }
+    cout<<endl;
+}
+
+
+
 int main() {
     cout<<"enter the size of array"<<endl;
     int size;
@@ -14,5 +33,8 @@ int main() {
     cin>>size1;
     array_init(arr,size1);
 
-    
+    int sum=arraySum(arr,size1);
+    printArr(arr,size1);
+
+    cout<<"sum of array elements is"<<sum<<endl;
 }
