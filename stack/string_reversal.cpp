2374c1966e70bed5a30b3439f2d43cde2981d949
kannan <kannanabhistudies@gmail.com>
2022-07-04 13:25:32 +0530

    string reversal done

---
+++ stack/string_reversal.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+#include<stack>
+#include<string>
+using namespace std;
+
+int main() {
+    stack<char> st;
+    string name="vigilant";
+
+    for(int i=0;i<name.size();i++) {
+        char ch=name[i];
+        st.push(ch);
+    }
+    string ans="";
+    while(!st.empty()) {
+        ans.push_back(st.top());
+        st.pop();
+    }
+    cout<<"answer is "<<ans<<endl;
+    return 0;
+
+}
