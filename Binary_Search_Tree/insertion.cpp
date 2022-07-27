179b976d77bbda878e3e54b96c1544f2120b14a1
kannan <kannanabhistudies@gmail.com>
2022-07-27 23:18:56 +0530

    insertion complete

--- Binary_Search_Tree/insertion.cpp
+++ Binary_Search_Tree/insertion.cpp
@@ -1,4 +1,5 @@
 #include<iostream>
+#include<queue>
 using namespace std;
 
 class Node {
@@ -13,23 +14,68 @@ public:
         this->right=NULL;
     }
 };
-void insertIntoTree(Node* root,int value) {
-
-    return;
+Node* insertIntoTree(Node* root,int value) {
+    //base case
+    if(root==NULL) {
+        root=new Node(value);
+        return root;
+    }
+    //if the value is greater insert to right sub-tree
+    if(value>root->data) {
+        root->right=insertIntoTree(root->right,value);
+    }
+    //if the value is smaller insert to left sub-tree
+    else if(value<root->data) {
+        root->left=insertIntoTree(root->left,value);
+    }
+    return root;
 }
 void buildTree(Node* &root) {
     int value;
     cin>>value;
     while(value!=-1) {
-        insertIntoTree(root,value);
+        root=insertIntoTree(root,value);
         cin>>value;
     }
+}
+void levelOrderTraversal(Node* root) {
+
+    queue<Node*> q;
+    q.push(root);
+    q.push(NULL);
+
+    while(!q.empty()) {
+        Node* temp=q.front();
+        q.pop();
+
+        if(temp==NULL) {
+            cout<<endl;
+            if(!q.empty()) 
+                q.push(NULL);
+
+        }
+        else {
+            cout<<temp->data<<" ";
+            if(temp->left) 
+                q.push(temp->left);
+            if(temp->right)
+                q.push(temp->right);
+            
+        }
+    }
+
 }
 
 int main() {
     Node* root=NULL;
     cout<<"intput the elements to be entered"<<endl;
+    /* test case for input
+    10 8 21 7 27 5 4 3 -1
+    */
     buildTree(root);
+
+    //level order traversal for checking
+    levelOrderTraversal(root);
 
     return 1;
 }
